5eba1bd07e5f8369bd56fc868567a2eab9b702b7
Eduardo Lima <duduzx02@gmail.com>
2023-12-07 03:24:37 -0300

    Um sistema para saber qual combustivél está em vantagem

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int valorFixo = 120;
+
+        System.out.println("Nome do carro: ");
+        String nomeCarro = scanner.nextLine();
+
+        System.out.println("Qual o preço da Gasolia: ");
+        double precoGasolina = scanner.nextDouble();
+
+        System.out.println("Qual o preço do Etanol: ");
+        double precoEtanol = scanner.nextDouble();
+
+        System.out.println("Seu carro faz quantos km com 1 litro de Gasolina: ");
+        double kmPorLitroGasolina = scanner.nextDouble();
+
+        System.out.println("Seu carro faz quantos km com 1 litro de Etanol? ");
+        double kmPorLitroEtanol = scanner.nextDouble();
+
+        double litrosDeGasolinaComValorFixo = valorFixo / precoGasolina;
+        double litroDeEtanolComValorFixo = valorFixo / precoEtanol;
+
+        double kmTotalComGasolina = litrosDeGasolinaComValorFixo * kmPorLitroGasolina;
+        double  kmTotalComEtanol = litroDeEtanolComValorFixo * kmPorLitroEtanol;
+
+        System.out.printf("%.2f km%n", kmTotalComGasolina);
+        System.out.printf("%.2f km%n ", kmTotalComEtanol);
+
+        if(kmTotalComGasolina > kmTotalComEtanol){
+            System.out.println("A vantagem " + nomeCarro + " está com a Gasolina");
+        }  else {
+            System.out.println("A vantagem " + nomeCarro + " está com o Etanol.");
+        }
+
+    }
+}
